b1bc0c3b75380ac51eee015eec94de5466b199a7
for-hygge <luna.yl827@gmail.com>
2024-07-29 16:23:43 -0500

    anecdotes exercise

--- src/PartOne.jsx
+++ src/PartOne.jsx
@@ -1,5 +1,40 @@
 import { useState } from "react";
 
+const Anecdotes = ({ selected, votes, handleVotes, getRandomAnecdote }) => {
+    const anecdoteList = [
+      'If it hurts, do it more often.',
+      'Adding manpower to a late software project makes it later!',
+      'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+      'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+      'Premature optimization is the root of all evil.',
+      'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+      'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+      'The only way to go fast, is to go well.'
+    ];
+
+    const maxVotes = Math.max(...votes);
+    const maxVotesIndex = votes.indexOf(maxVotes);
+
+    return (
+    <div>
+        <div>
+        <h1>Anecdote of the Day</h1>
+        <p>{anecdoteList[selected]}</p>
+        <p>has {votes[selected]} votes</p>
+        <div>
+            <button onClick={() => handleVotes(selected)}>vote</button>
+            <button onClick={getRandomAnecdote}>next anecdote</button>
+        </div>
+        </div>
+        <div>
+        <h1>Anecdote with most votes</h1>
+        <p>{anecdoteList[maxVotesIndex]}</p>
+        <p>has {maxVotes} votes</p>
+        </div>
+    </div>
+    );
+}
+
 const Button = ({ desc, handleClicks}) => {
     return (
         <button onClick={() => handleClicks(desc)}>{desc}</button>
@@ -47,6 +82,9 @@ const Statistics = (props) => {
 }
 
 const PartOne = () => {
+    const anecdoteListLength = 8;
+    const [selected, setSelected] = useState(0);
+    const [votes, setVotes] = useState(Array(anecdoteListLength).fill(0));
     const [clicks, setClicks] = useState({ good: 0, neutral: 0, bad: 0 });
     const [totalClicks, setTotalClicks] = useState(0);
 
@@ -62,6 +100,16 @@ const PartOne = () => {
     const reset = () => {
         setClicks({ good: 0, neutral: 0, bad: 0 });
         setTotalClicks(0);
+    }
+
+    const handleVotes = (selected) => {
+        const newVotes = [...votes];
+        newVotes[selected] += 1;
+        setVotes(newVotes);
+    }
+
+    const getRandomAnecdote = () => {
+        setSelected(Math.floor(Math.random() * anecdoteListLength));
     }
 
     return (
@@ -81,8 +129,14 @@ const PartOne = () => {
             <p>positive {totalClicks===0? 0: clicks.good / (clicks.good + clicks.neutral + clicks.bad)}%</p> */}
             <h1>statistics</h1>
             <Statistics clicks={clicks} totalClicks={totalClicks}/>
+            <Anecdotes 
+                selected={selected} 
+                votes={votes} 
+                handleVotes={handleVotes} 
+                getRandomAnecdote={getRandomAnecdote}
+            />
         </div>
     );
-}
+};
 
 export default PartOne;
